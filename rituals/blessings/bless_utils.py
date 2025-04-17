acbe8b0ca5444ba35eec90288a9c0a9bc67955d4
vandigital <vandigital@gmail.com>
2025-04-17 19:18:38 +0800

    fixed filepath typo

--- rituals/blessings/bless_utils.py
+++ rituals/blessings/bless_utils.py
@@ -3,6 +3,6 @@ import json
 
 def log_blessing(blessing, folder="rituals/blessings"):
     os.makedirs(folder, exist_ok=True)
-    filepath = os.path.join(folder, "bless_log.jsonl")
+    filepath = os.path.join(folder, "bless_log.json1")
     with open(filepath, "a") as f:
         f.write(json.dumps(blessing) + "\n")
